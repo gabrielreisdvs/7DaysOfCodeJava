856b116b3cf8291003ea5909cad0c27e8b09d387
gabrielreisdvs <gabrielreis1994@gmail.com>
2022-08-11 17:23:42 -0300

    Day2. Parsing the json of the movies list.

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,53 @@
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws Exception {
+        String url = "https://imdb-api.com/en/API/Top250Movies/k_lo1tm5nf";
+
         var client = HttpClient.newHttpClient();
 
         var request = HttpRequest
-                                    .newBuilder(URI.create("https://imdb-api.com/en/API/Top250Movies/<API_KEY>"))
+                                    .newBuilder(URI.create(url))
                                     .GET()
                                     .build();
 
-        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+        // ASSYNC
+        /*
+         client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
                 .thenApply(HttpResponse::body)
-                .thenAccept(System.out::println)
+                .thenAccept(String::toString)
                 .join();
+
+         */
+
+        //SYNC
+
+        String json = client.send(request, HttpResponse.BodyHandlers.ofString()).body();
+
+        //System.out.println(json);
+
+        var parser = new JsonParser();
+
+        List<Map<String, String>> ListaFilmes = parser.parse(json);
+
+        List<String> titles = new ArrayList<>();
+        List<String> UrlImages = new ArrayList<>();
+        List<String> rates = new ArrayList<>();
+
+
+        for (Map<String, String> filme: ListaFilmes) {
+            titles.add(filme.get("title"));
+            UrlImages.add(filme.get("image"));
+            rates.add(filme.get("imDbRating"));
+            //System.out.println(filme);
+        }
+
+
     }
 }
